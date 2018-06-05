4bfe5a78be057fd1b6f73e02f50a92673c3fc2cd
Josh B <josh.s.brown@hotmail.com>
2018-06-05 03:31:18 +0000

    make the "walk" better

--- walk.py
+++ walk.py
@@ -3,10 +3,11 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 left = 11
-right = 4
+right = 2.5
 center = 7
 
 freq = 50
+time.sleep(2)
 
 GPIO.setup(12, GPIO.OUT)
 GPIO.setup(35, GPIO.OUT)
@@ -14,19 +15,21 @@ GPIO.setup(35, GPIO.OUT)
 pwm1 = GPIO.PWM(12, freq)
 pwm2 = GPIO.PWM(35, freq)
 
-pwm1.start(right)
-pwm2.start(right)
-time.sleep(.2)
+pwm1.start(3.5)
+pwm2.start(3.5)
+
+
+time.sleep(1)
 
 def stepup(pwm1, pwm2):
     time.sleep(.3)
-    pwm1.ChangeDutyCycle(3)
-    pwm2.ChangeDutyCycle(3)
+    pwm1.ChangeDutyCycle(4)
+    pwm2.ChangeDutyCycle(4)
 
 def stepdown(pwm1, pwm2):
     time.sleep(.3)
     pwm1.ChangeDutyCycle(5)
-    pwm2.ChangeDutyCycle(6)
+    pwm2.ChangeDutyCycle(5)
 
 
 def fullstep(pwm1, pwm2):
@@ -39,6 +42,9 @@ def fullstep(pwm1, pwm2):
 #    print 'x'
 
 
+fullstep(pwm1, pwm2)
+fullstep(pwm1, pwm2)
+fullstep(pwm1, pwm2)
 fullstep(pwm1, pwm2)
 fullstep(pwm1, pwm2)
 fullstep(pwm1, pwm2)
